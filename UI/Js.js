85670af61f0127e66a9d3e3e58ede4a5a41a9d8a
mozzy11 <mozzymutesa@gmail.com>
2018-08-09 08:09:28 -0600

    2nd commit

--- UI/Js.js
+++ UI/Js.js
@@ -1,18 +1,40 @@
-function field_focus(field, email)
-  {
-    if(field.value == email)
-    {
-      field.value = '';
+
+
+  function validateForm() {
+    var x = document.forms["LoginForm"]["user"].value;
+    var x2 = document.forms["LoginForm"]["email"].value;
+    if (x == "") {
+        alert("Name must be filled out");
+        return false;
     }
+    if (x2 == "") {
+      alert("Email must be filled out");
+      return false;
   }
+  
+}
 
-  function field_blur(field, email)
-  {
-    if(field.value == '')
-    {
-      field.value = email;
-    }
+function validateSignupForm() {
+  var x = document.forms["SignupForm"]["user"].value;
+  var x2 = document.forms["SignupForm"]["email"].value;
+  var x3 = document.forms["SignupForm"]["pass1"].value;
+  var x4 = document.forms["SignupForm"]["pass2"].value;
+  if (x == "") {
+      alert("Name must be filled out");
+      return false;
   }
+  if (x2 == "") {
+    alert("Email must be filled out");
+    return false;
+}
+if (x3 == "") {
+  alert("Set Pasword");
+  return false;
+}
+if (!x3 == x4) {
+  alert("Password Mismatch");
+  return false;
+}
 
 
-	});
+}
