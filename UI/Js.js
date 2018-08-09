889b02e8315bafcc1334169ef1d86bcedf589fbe
mozzy11 <mozzymutesa@gmail.com>
2018-08-09 01:49:01 -0600

    commit1 for login and signup pages[#159634778]

---
+++ UI/Js.js
@@ -0,0 +1,18 @@
+function field_focus(field, email)
+  {
+    if(field.value == email)
+    {
+      field.value = '';
+    }
+  }
+
+  function field_blur(field, email)
+  {
+    if(field.value == '')
+    {
+      field.value = email;
+    }
+  }
+
+
+	});
