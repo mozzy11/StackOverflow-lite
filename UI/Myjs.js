d4555a927edf41c0eed3c4514ecef5f070d94f42
mozzy11 <mozzymutesa@gmail.com>
2018-08-09 08:31:51 -0600

    2nd commit

--- UI/Myjs.js
+++ UI/Myjs.js
@@ -1,5 +1,32 @@
 
-function addComment() {
-    document.createElement("input");
-    var container = document.getElementById("container");
-}
+ function validatePostForm() {
+    var x = document.forms["PostForm"]["querry"].value;
+
+    if (x == "") {
+        alert("Empty Queston .Please Post a question");
+        return false;
+    }
+    
+  }
+
+
+  function ValidAns() {
+    var x = document.forms["ans"]["answer"].value;
+
+    if (x == "") {
+        alert("Empty Answer .Please Post an Answer");
+        return false;
+    }
+    
+  }
+
+  
+  function ValdAsses(){
+    var x = document.forms["asses"]["comment"].value;
+
+    if (x == "") {
+        alert("Empty Comment .Please Post a comment");
+        return false;
+    }
+    
+  }
