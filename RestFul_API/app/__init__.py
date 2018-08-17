226b4ddbb9b4170a0d41556b542042b88c8a127d
mozzy11 <mozzymutesa@gmail.com>
2018-08-17 09:21:59 -0600

    added tests

---
+++ RestFul_API/app/__init__.py
@@ -0,0 +1,61 @@
+from flask import Flask, jsonify,request,abort, Response, json
+
+# local import
+from instance.config import app_config
+
+# initialize sql-alchemy
+
+
+def create_app(config_name):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object(app_config[config_name])
+    app.config.from_pyfile('config.py')
+
+    qtns = [
+        {
+            'id': 101,
+            'txt': 'what is Andela',
+            'Poster': 'mozzy'
+        },
+        {
+            'id': 201,
+            'txt': 'what is a boot camp',
+            'Poster': 'Arnold'
+        }
+    ]
+
+    # POST An answer
+
+    @app.route('/questions', methods=['POST'])
+    def add_qtn():
+
+        request_qtn = request.json
+        if (valid_qtn(request_qtn)):
+            qtn = {
+                'id': request_qtn['id'],
+                'txt': request_qtn['txt'],
+                'Poster': request_qtn['Poster']
+            }
+            qtns.append(qtn)
+            response = Response("", 201, mimetype="application/json")
+            response.headers['Location'] = "books/" + str(request_qtn['id'])
+            print(qtns)
+            return jsonify(qtn)
+            # return response
+        else:
+            bad_object = {
+                "error": "Invalid book object",
+                "help_string":
+                    "Request format should be {'id': 1,'txt': 'Where is Andela','Poster': 'Seruwu' }"
+
+            }
+            response = Response(json.dumps(bad_object), status=400, mimetype="appliation/json")
+            return response
+
+    def valid_qtn(qtnObj):
+        if "id" in qtnObj and "txt" in qtnObj and "Poster" in qtnObj:
+            return True
+        else:
+            return False
+
+    return app
---
+++ RestFul_API/test_PostQuestion.py
@@ -0,0 +1,27 @@
+import unittest
+import json
+from app import create_app
+
+class StackTestCase(unittest.TestCase):
+
+
+    def setUp(self):
+        """Define test variables and initialize app."""
+        self.app = create_app(config_name="testing")
+        self.client = self.app.test_client
+
+
+    def test_add_ans(self):
+        respon = self.client().post( 'questions',data = json.dumps({
+            'id': 202,
+            'txt': 'is this queston Returned',
+            'Poster': 'Arnold'
+                    } ) ,content_type='application/json', )
+        data = json.loads(respon.get_data(as_text=True))
+        assert respon.status_code == 200
+        assert data['id'] == 202
+
+
+
+if __name__ == "__main__":
+    unittest.main()
