5a78575b33f24efe3d38eebe46342a58e07df137
mozzy11 <mozzymutesa@gmail.com>
2018-08-20 15:27:25 -0600

    Added an Index Page

---
+++ UI/ans.js
@@ -0,0 +1,50 @@
+
+ function validatePostForm() {
+    var x = document.forms["PostForm"]["querry"].value;
+
+    if (x == "") {
+        alert("Empty Queston .Please Post a question");
+        return false;
+    } else {
+        window.location.replace ("answerPage.html");
+    }
+    
+  }
+
+
+  function ValidAns() {
+    var x = document.forms["ans"]["answer"].value;
+
+    if (x == "") {
+        alert("Empty Answer .Please Post an Answer");
+        return false;
+    }
+    
+  }
+
+  
+  function ValdAsses(){
+    var x = document.forms["asses"]["comment"].value;
+
+    if (x == "") {
+        alert("Empty Comment .Please Post a comment");
+        return false;
+    } 
+}
+function  delet(){
+    var checkbox = document.getElementById("chbx");
+   
+       if(checkbox.checked){
+        var retVal = confirm("Do you want to Delete this Question ?");
+               if( retVal == true ){
+                alert("Question Deleted");
+               
+                  return true;
+               }
+               else{
+                  return false;
+
+}
+}
+}
+
